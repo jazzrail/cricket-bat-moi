d81c1b461b2f6ef1f95a32fc932ab9f9e5a0834e
jazzrail <jazzrail@hotmail.com>
2023-01-04 11:33:44 +0000

    Added <sup></sup> to calculation output

--- js/calc.js
+++ js/calc.js
@@ -1,46 +1,44 @@
 document.getElementById('btn-calc').addEventListener('click', (e) => {
-  e.preventDefault();
+	e.preventDefault();
 
-  const handleLen = parseFloat(document.querySelector('#handleLen').value);
-  if (document.getElementById('handleLen').value.length == 0) {
-    alert('Please enter integer or decimal in each of the fields');
-    document.classList.remove('empty-field');
-  }
+	const handleLen = parseFloat(document.querySelector('#handleLen').value);
+	if (document.getElementById('handleLen').value.length == 0) {
+		alert('Please enter integer or decimal in each of the fields');
+		document.classList.remove('empty-field');
+	}
 
-  const bladeLen = parseFloat(document.querySelector('#bladeLen').value);
-  if (document.getElementById('bladeLen').value.length == 0) {
-    alert('Please enter integer or decimal in each of the fields');
-    document.classList.remove('empty-field');
-  }
-  const batMass = parseFloat(document.querySelector('#batMass').value);
-  if (document.getElementById('batMass').value.length == 0) {
-    alert('Please enter integer or decimal in each of the fields');
-    document.classList.remove('empty-field');
-  }
+	const bladeLen = parseFloat(document.querySelector('#bladeLen').value);
+	if (document.getElementById('bladeLen').value.length == 0) {
+		alert('Please enter integer or decimal in each of the fields');
+		document.classList.remove('empty-field');
+	}
+	const batMass = parseFloat(document.querySelector('#batMass').value);
+	if (document.getElementById('batMass').value.length == 0) {
+		alert('Please enter integer or decimal in each of the fields');
+		document.classList.remove('empty-field');
+	}
 
-  const balPoint = parseFloat(document.querySelector('#balPoint').value);
-  if (document.getElementById('balPoint').value.length == 0) {
-    alert('Please enter integer or decimal in each of the fields');
-    document.classList.remove('empty-field');
-  }
+	const balPoint = parseFloat(document.querySelector('#balPoint').value);
+	if (document.getElementById('balPoint').value.length == 0) {
+		alert('Please enter integer or decimal in each of the fields');
+		document.classList.remove('empty-field');
+	}
 
-  const res = document.getElementById('res');
+	const res = document.getElementById('res');
 
-  // 1
-  const bladeMass =
-    (batMass * handleLen - 2 * batMass * balPoint) / (-handleLen - bladeLen);
-  // 2
-  const handleMass = batMass - bladeMass;
-  // 3
-  let finalCalculation =
-    (handleMass * handleLen ** 2) / 3 +
-    ((bladeMass * bladeLen ** 2) / 12 +
-      bladeMass * (handleLen + bladeLen / 2) ** 2);
+	// 1
+	const bladeMass = (batMass * handleLen - 2 * batMass * balPoint) / (-handleLen - bladeLen);
+	// 2
+	const handleMass = batMass - bladeMass;
+	// 3
+	let finalCalculation =
+		(handleMass * handleLen ** 2) / 3 +
+		((bladeMass * bladeLen ** 2) / 12 + bladeMass * (handleLen + bladeLen / 2) ** 2);
 
-  let result = finalCalculation.toFixed(3);
+	let result = finalCalculation.toFixed(3);
 
-  res.innerHTML = `The MOI =
-		${result} Kg-m2`;
+	res.innerHTML = `The MOI =
+		${result} Kg-m<sup>2</sup>`;
 });
 
 // 	res.innerHTML = `The MOI is:
